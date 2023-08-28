a5dc3068bb040f186bf719abe2a1e44617f7a78e
MDAbuSaad <143396130+MDAbuSaad@users.noreply.github.com>
2023-08-29 00:04:53 +0600

    Add files via upload

---
+++ matchresult.blade.php
@@ -0,0 +1,27 @@
+<?php 
+include("./header.blade.php");
+
+?>
+
+<div class ='container mt-5'>
+    <h2 class = 'text-center'>Match Results</h2>
+    <div class ='card-deck mt-4'>
+        <a href="#" class ='btn' style='text-align:left; padding:0px; margin:0px;'>
+           <div class ="card">
+            <img src="images/result.png" class ='card-img-top' alt="Mbappe" />
+            <div class ="card body">
+                <h5 class ='card-title'></h5>
+                <p class ='card-text'></p>
+            </div>
+
+            <div class="card-footer">
+                <a class ="btn btn-primary text-white font-weight-bolder float-right"href ="#"> </a>
+
+
+            </div>
+           </div>
+        </a>
+
+    </div>
+
+</div>
